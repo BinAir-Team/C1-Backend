1a60f17f3655d5a644bdba262e4fbaaf58381ad6
Afif Ardhiansyah <afifardhiansyah@gmail.com>
2022-11-23 23:36:43 +0700

    add started file

---
+++ config/routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const controllers = require('../app/controllers');
+
+router.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const prefix = '/api/v1';
+
+//user api
+// router.get(prefix + '/users', controllers.authControllers.getAllUsers);
+
+module.exports = router;
