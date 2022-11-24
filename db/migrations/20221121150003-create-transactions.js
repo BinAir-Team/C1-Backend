951c615423eee32a86e1a22f7e2fb64a33284041
AsrofurRizqi <asrofurrizqi@gmail.com>
2022-11-25 00:23:59 +0700

    fix typo

--- db/migrations/20221121150003-create-transactions.js
+++ db/migrations/20221121150003-create-transactions.js
@@ -14,7 +14,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.UUID
       },
-      tiketsId: {
+      ticketsId: {
         allowNull: false,
         type: Sequelize.UUID
       },
