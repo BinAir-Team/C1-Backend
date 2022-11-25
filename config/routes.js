a3750b6f3b5c8b239bbd9931f36877ac76be4b92
adeeep14 <disconetct@gmail.com>
2022-11-25 19:39:54 +0700

    authMiddleware Done

--- config/routes.js
+++ config/routes.js
@@ -1,13 +1,25 @@
 const router = require('express').Router();
-const { authController } = require('../controllers');
+const { verifyToken, verifyRefreshToken, verifyAdmin } = require('../middleware/authMiddleware');
+const {registerMember ,login, logout, refreshToken} = require('../controllers/authController');
+const {getUserData, updateUserData} = require('../controllers/userController');
 
 
 router.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-const prefix = '/api/v1';
 
 // auth routes
+router.post('/register', registerMember);
+router.post('/login', login);
+router.post('/logout', verifyToken, logout);
+
+// refresh token route
+router.post('/refresh-token', verifyRefreshToken, refreshToken);
+
+// user routes
+router.get('/user', verifyToken, getUserData);
+router.put('/user', verifyToken, updateUserData);
+
 
 module.exports = router;
