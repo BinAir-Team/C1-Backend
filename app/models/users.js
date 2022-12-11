4b8d379a21c056ca690381b931155d96b8ad4a1f
Muhammad Afif Dwi Ardhiansyah <87472849+AfifArdhiansyah@users.noreply.github.com>
2022-12-11 10:21:15 +0700

    Feature/email verification (#42)
    fix post ticket

--- app/models/users.js
+++ app/models/users.js
@@ -13,6 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       this.hasMany(models.wishlists, { foreignKey: "usersId" });
     }
   }
+<<<<<<< HEAD
   users.init({
     firstname: DataTypes.STRING,
     lastname: DataTypes.STRING,
@@ -20,6 +21,7 @@ module.exports = (sequelize, DataTypes) => {
     email: DataTypes.STRING,
     verified: DataTypes.BOOLEAN,
     password: DataTypes.STRING,
+    refresh_token: DataTypes.TEXT,
     phone: DataTypes.STRING,
     role: DataTypes.STRING,
     profile_image: DataTypes.STRING
@@ -27,5 +29,23 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'users',
   });
+=======
+  users.init(
+    {
+      firstname: DataTypes.STRING,
+      lastname: DataTypes.STRING,
+      gender: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      phone: DataTypes.STRING,
+      role: DataTypes.STRING,
+      profile_image: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "users",
+    }
+  );
+>>>>>>> 1bbf91599d773a9bd10c6ecd05d5e647662cf682
   return users;
 };
