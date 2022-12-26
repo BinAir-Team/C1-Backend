19e22e5c3cc4f03a72d649c4d1b4d61a0a5a1f86
adeeep14 <disconetct@gmail.com>
2022-12-26 20:45:00 +0700

    read promo add pagination by user

--- config/routes.js
+++ config/routes.js
@@ -141,7 +141,7 @@ router.delete(
 );
 
 // promo routes for user
-router.get(prefix + "/promos", getAllPromos);
+router.get(prefix + "/promos", getAllPromosWithPagination);
 router.get(prefix + "/promo/:id", getPromoById);
 
 // admin CRUD promo routes
